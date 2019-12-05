492ebdbeffbef23ac64c3f43764c9fa0ad1833b3
zoe-zachary <zoezachary27@gmail.com>
2019-12-05 20:52:58 +0700

    push buat branching dari sini

---
+++ src/views/profileList.vue
@@ -0,0 +1,34 @@
+<template>
+   <div id="profile-list" class="container mt-3">
+      <div class="row">
+         <ProfileCard v-for="(profileCard, i) in profileCards" :key="i" :profileCards="profileCards"></ProfileCard>
+      </div>
+   </div>
+  
+</template>
+
+<script>
+import ProfileCard from '../components/card.vue'
+import axios from 'axios'
+
+export default {
+   // props: [],
+   data: {
+      profileCards: [{
+
+      }]
+   },
+   components: {
+      ProfileCard
+   },
+   methods: {
+      fetchProfiles() {
+
+      }
+   }
+}
+</script>
+
+<style>
+
+</style>
