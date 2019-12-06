c69528d8ab8a5bea1b1e668e427b7a0d0fc1733c
anggabanny <anggabanny@gmail.com>
2019-12-06 07:49:08 +0700

    finish_build_ProfilePage_and_Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
 <template>
-
+<div>
+    <app-navbar></app-navbar>
+    <app-profilePage></app-profilePage>
+</div>
 </template>
 
 <script>
+import Navbar from './views/Navbar'
+import ProfilePage from './views/ProfilePage'
 export default {
+    name : 'app',
+    data(){
+        return {
 
+        }
+    },
+    components : {
+        'app-profilePage' : ProfilePage,
+        'app-navbar' : Navbar
+    }
 }
 </script>
 
 <style>
-
 </style>
---
+++ src/views/Navbar.vue
@@ -0,0 +1,36 @@
+<template>
+<div>
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" style="background-color: palevioletred;">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <a class="navbar-brand ml-5"  style="color: white;" href="#">
+                <i class="fas fa-heartbeat fa-2x" style="transform: rotate(-45deg);"></i>
+                <span style="margin-left: -13px; ;font-size: 30px; font-weight: bold; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;"> LOVAPP </span>
+            </a>
+            <ul class="navbar-nav mx-auto">
+                <button type="button" class="ml-5 btn btn-light mr-5">
+                    <i class="fas fa-arrow-left    "></i>
+                    Back
+                </button>
+            </ul>
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0"> </ul>
+            <button type="button" class="btn btn-light mr-5" >
+                Logout
+                <i class="fas fa-sign-out-alt"></i>
+            </button>
+        </div>
+    </nav>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
