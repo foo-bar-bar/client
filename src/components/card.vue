67ea6dc31eff161a9ab75bd8ff2ffbf4281ea57e
zoe-zachary <zoezachary27@gmail.com>
2019-12-06 10:13:34 +0700

    profile list and card updated

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,19 +1,21 @@
 <template>
-   <div>
-      <div class="card" style="width: 18rem;">
-            <img src="../../assets/cat1.jpg" class="card-img-top border-bottom border-dark" alt="cat">
-            <div class="card-body">
-               <h5 class="card-title">Dedi Prakasa</h5>
-               <p class="card-text">Aku mencari wanita cantik yang bisa memberiku kehangatan.</p>
-               <a href="#" class="btn btn-primary">Cek profileku!</a>
-            </div>
+  <!-- card -->
+  <div class="card border border-grey m-3 shadow-sm p-3 mb-1 bg-white rounded" style="width: 18rem;">
+      <img src="../../assets/cat1.jpg" class="card-img-top border-bottom border-dark pb-2" alt="cat">
+      <div class="card-body d-flex flex-column">
+         <h5 class="card-title">{{profileCard.name}}</h5>
+         <p class="card-text">{{profileCard.profileDescription}}</p>
+         <div class="justify-self text-center">
+
+            <a href="#" class="btn btn-primary text-center">Cek profileku!</a>
          </div>
+      </div>
    </div>
 </template>
 
 <script>
 export default {
-   props: ['profileCards']
+   props: ['profileCard']
 }
 </script>
 
